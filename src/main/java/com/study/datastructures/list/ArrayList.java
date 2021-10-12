a43551ff5577fe8c0bce565ee70a1ef9b85b1944
Maks <maksim.stepanok@gmail.com>
2021-10-12 23:45:23 +0300

    Lets start

---
+++ src/main/java/com/study/datastructures/list/ArrayList.java
@@ -0,0 +1,123 @@
+package com.study.datastructures.list;
+
+public class ArrayList implements List {
+    private static final int INIT_SIZE = 10;
+    Object[] array = new Object[INIT_SIZE];
+    private int pointer = 0;
+
+    public void add(Object value) {
+        if(pointer >= array.length){
+            resize(array.length*2);
+        }
+        array[pointer++] = value;
+    }
+
+    public void add(Object value, int index) {
+        if (index >= pointer || index < 0) {
+            throw new IndexOutOfBoundsException("ADD. Index should be between 0 and " + pointer + ". You entered the index value " + index + ". Sorry...");
+        }
+        if(pointer >= array.length){
+            resize(array.length*2);
+        }
+        for (int i = index; i < pointer; i++){
+            array[i+1] = array[i];
+        }
+        pointer++;
+        array[index] = value;
+    }
+
+    public Object remove(int index) {
+        if (index >= pointer || index < 0) {
+            throw new IndexOutOfBoundsException("REMOVE. Index should be between 0 and "+(pointer-1)+". You entered the index value "+index+". Sorry...");
+        }
+        for (int i = index; i < pointer-1; i++){
+            array[i] = array[i+1];
+        }
+        pointer--;
+        resize(array.length-1);
+        return array;
+    }
+
+    public Object get(int index) {
+        if (index >= pointer || index < 0) {
+            throw new IndexOutOfBoundsException("GET. Index should be between 0 and "+(pointer-1)+". You entered the index value "+index+". Sorry...");
+        }
+        return array[index];
+    }
+
+    public Object set(Object value, int index) {
+        if (index >= pointer || index < 0) {
+            throw new IndexOutOfBoundsException("SET. Index should be between 0 and "+(pointer-1)+". You entered the index value "+index+". Sorry...");
+        }
+        return array[index] = value;
+    }
+
+    public void clear() {
+        for(int i = pointer-1; i > 0; i--){
+            array[i] = null;
+        }
+        pointer=0;
+        resize(INIT_SIZE);
+    }
+
+    public int size() {
+        return pointer;
+    }
+
+    public boolean isEmpty() {
+        return pointer == 0;
+    }
+
+    public boolean contains(Object value) {
+        if(pointer > 0){
+            for (int i = 0; i < pointer; i++) {
+                if( array[i].equals(value) ){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public int indexOf(Object value) {
+        if(pointer > 0){
+            for (int i = 0; i < pointer; i++) {
+                if( array[i].equals(value) ){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(Object value) {
+        int marker = -1;
+        if( pointer > 0 ){
+            for ( int i = 0; i < pointer; i++ ) {
+                if(array[i].equals(value)){
+                    marker=  i;
+                }
+            }
+        }
+        return marker;
+    }
+
+    @Override
+    public String toString(){
+        String str="[";
+        for (int i = 0; i < pointer; i++) {
+            str+=array[i];
+            if(i<pointer-1){
+                str+=",";
+            }
+        }
+        str+="]";
+        return str;
+    }
+
+    private void resize(int newLength) {
+        Object[] newArray = new Object[newLength];
+        System.arraycopy(array, 0, newArray, 0, pointer);
+        array = newArray;
+    }
+}
---
+++ src/test/java/com/study/datastructures/list/ArrayListTest.java
@@ -0,0 +1,175 @@
+package com.study.datastructures.list;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ArrayListTest {
+
+    @Test
+    public void add() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        Assert.assertEquals(3,arrayList.size());
+    }
+
+    @Test
+    public void testAdd() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        arrayList.add("D",2);
+        Assert.assertEquals(4,arrayList.size());
+        Assert.assertEquals("D",arrayList.get(2));
+    }
+
+    @Test
+    public void remove() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        arrayList.remove(1);
+        Assert.assertEquals(2,arrayList.size());
+        Assert.assertEquals("C",arrayList.get(1));
+
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void removeExp() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.remove(1);
+    }
+
+    @Test
+    public void get() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        Assert.assertEquals("C",arrayList.get(2));
+    }
+
+    @Test
+    public void set() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        arrayList.set("F",1);
+        Assert.assertEquals("A",arrayList.get(0));
+        Assert.assertEquals("F",arrayList.get(1));
+        Assert.assertEquals("C",arrayList.get(2));
+
+    }
+
+    @Test
+    public void clear() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        Assert.assertEquals(15,arrayList.size());
+        arrayList.clear();
+        Assert.assertEquals(0,arrayList.size());
+    }
+
+    @Test
+    public void size() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        Assert.assertEquals(15,arrayList.size());
+    }
+
+    @Test
+    public void isEmpty() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        arrayList.add("C");
+        Assert.assertFalse(arrayList.isEmpty());
+    }
+
+    @Test
+    public void contains() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        Assert.assertTrue(arrayList.contains("C"));
+        Assert.assertFalse(arrayList.contains("F"));
+    }
+
+    @Test
+    public void indexOf() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        Assert.assertEquals(2,arrayList.indexOf("C"));
+        Assert.assertEquals(-1,arrayList.indexOf("F"));
+    }
+
+    @Test
+    public void lastIndexOf() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        arrayList.add("F");
+        arrayList.add("B");
+        arrayList.add("H");
+        Assert.assertEquals(4,arrayList.lastIndexOf("B"));
+    }
+
+    @Test
+    public void testToString() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        Assert.assertEquals("[A,B,C]",arrayList.toString());
+    }
+}
