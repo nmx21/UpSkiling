ebcff9eeb22aec3e57a4817cf7401e3adf0a58a0
nmx21 <maksim.stepanok@gmail.com>
2021-11-03 22:56:33 +0200

    Some fix in SocketServer

--- src/main/java/com/luxoft/socket/SocketServer.java
+++ src/main/java/com/luxoft/socket/SocketServer.java
@@ -1,8 +1,6 @@
 package com.luxoft.socket;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,20 +9,25 @@ public class SocketServer {
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(PORT);
-        Socket socket = serverSocket.accept();
+        try {
+            Socket socket = serverSocket.accept();
 
-        InputStream inputStream = socket.getInputStream();
-        byte[] array = new byte[50];
-        int count = inputStream.read(array);
-        String inputStr = new String(array, 0, count);
+            byte[] array = new byte[50];
+            BufferedInputStream inputStream = new BufferedInputStream(socket.getInputStream());
+            BufferedOutputStream outputStream = new BufferedOutputStream(socket.getOutputStream());
 
+            int count;
+            while ((count = inputStream.read(array))!=-1){
+                String reciveMessage = new String(array,0,count);
+                outputStream.write(("Echo : "+reciveMessage).getBytes());
+                outputStream.flush();
+            }
 
-        OutputStream outputStream = socket.getOutputStream();
-        String testString = "Echo : " + inputStr;
-        outputStream.write(testString.getBytes());
-        inputStream.close();
-        outputStream.close();
-        socket.close();
-
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
